a096fa4005fd199070395ab89a53fe23e9026056
Rory Pearson <52637194+Mezmerizxd@users.noreply.github.com>
2022-10-25 19:39:04 +0100

    Delete release directory

--- release/server/api/v1/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const tests_1 = require("./tests");
-const r = (0, express_1.Router)();
-r.use('/tests', tests_1.default);
-exports.default = r;
--- release/server/api/v1/tests/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const demo_1 = require("./demo");
-const r = (0, express_1.Router)();
-r.get('/demo', demo_1.default.Perform);
-exports.default = r;
